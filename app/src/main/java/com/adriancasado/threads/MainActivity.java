07111fe7b42caddb6c101d28d073a17981bf210e
MarcArques <marc.arques.marimon@gmail.com>
2025-05-14 20:54:37 +0200

    Commit

---
+++ app/src/main/java/com/adriancasado/threads/MainActivity.java
@@ -0,0 +1,141 @@
+package com.adriancasado.threads;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    String error = "";
+
+    Button button;
+    ImageView imageView;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        // String responseData = getDataFromUrl("https://dadosabertos.camara.leg.br/api/v2/deputados");
+
+
+
+                        try {
+                            String responseData = getDataFromUrl("https://randomfox.ca/floof/");
+                            JSONObject obj = new JSONObject(responseData);
+                            String imageUrl = obj.getString("image");
+
+                            Log.i("IMAGE URL", imageUrl);
+
+                            try {
+                                InputStream in = new java.net.URL(imageUrl).openStream();
+                                bitmap = BitmapFactory.decodeStream(in);
+                            } catch (Exception e) {
+                                Log.e("Error", e.getMessage());
+                                e.printStackTrace();
+                            }
+
+                            Handler handler = new Handler(Looper.getMainLooper());
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if(bitmap != null) {
+                                        imageView.setImageBitmap(bitmap);
+                                    }
+
+                                }
+                            });
+                        } catch (JSONException e) {
+                            Log.e("ERROR", e.toString());
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+
+    private String getDataFromUrl(String demoIdUrl) {
+
+        String result = null;
+        int resCode;
+        InputStream in;
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            Log.i("RESPONSE CODE", String.valueOf(resCode));
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
